38362c18e4947f4cbb6faecd53adaac3ea64094f
Siti Marfungah <siti.marfungah.ft21@mail.umy.ac.id>
2022-10-07 17:04:20 +0700

    Deklarasi variable int untuk menyimpan banyaknya data pada array

--- Bubble_Sort/Program.cs
+++ Bubble_Sort/Program.cs
@@ -6,5 +6,8 @@ namespace Bubble_Sort
     {
         //Deklarasi array int dengan ukuran 20
         private int[] a = new int[20];
+
+        //Deklarasi variable int untuk menyimpan banyaknya data pada array
+        private int n;
     }
 }
